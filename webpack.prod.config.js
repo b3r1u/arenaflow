572bfd256c7817ea4192700fa8585d534c6c458c
b3r1u <roberioalbuquerque.neto@gmail.com>
2026-05-15 22:04:57 -0300

    feat: configurando remote de produção do MFE e restringe acesso ao admin da plataforma

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,18 @@
-module.exports = require('./webpack.config');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  remotes: {
+    "subscription-panel": "https://subscription-panel-eight.vercel.app/remoteEntry.js",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
